d6067a4ce9375ef822ee5f16bce2e7e07dce113b
JonGMS <joaoggabriell@gmail.com>
2026-06-25 15:57:29 -0300

    Salva alterações locais antes de trocar de branch

--- 1 - Apresentacao/obj/Debug/net9.0/Apresentacao.AssemblyInfo.cs
+++ 1 - Apresentacao/obj/Debug/net9.0/Apresentacao.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Apresentacao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7540822e22f36ee786482328de48ce1a5f31f42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fbf3040c5b63c394002dc05600cdc890175ecdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Apresentacao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Apresentacao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
